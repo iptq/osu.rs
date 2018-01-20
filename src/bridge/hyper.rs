7847d242dda05a2634c3e4937ffb3e262dcaa3f1
Zeyla Hellyer <zeylahellyer@gmail.com>
2018-01-20 15:31:36 -0800

    Update to hyper v0.11

--- src/bridge/hyper.rs
+++ src/bridge/hyper.rs
@@ -1,9 +1,21 @@
-use hyper::Client;
+use futures::{Future, Stream, future};
+use hyper::client::{Client, Connect};
+use hyper::{Error as HyperError, Uri};
 use serde_json;
 use std::collections::BTreeMap;
 use std::fmt::Write;
+use std::str::FromStr;
 use ::builder::*;
 use ::*;
+
+macro_rules! try_uri {
+    ($uri:ident) => {
+        match Uri::from_str(&$uri) {
+            Ok(v) => v,
+            Err(why) => return Box::new(future::err(Error::Uri(why))),
+        }
+    }
+}
 
 /// A trait used for implementation on various HTTP clients.
 pub trait OsuHyperRequester {
@@ -36,8 +48,10 @@ pub trait OsuHyperRequester {
     /// #     try_main().unwrap();
     /// # }
     /// ```
-    fn get_beatmaps<F>(&self, key: &str, f: F) -> Result<Vec<Beatmap>>
-        where F: FnOnce(GetBeatmapsRequest) -> GetBeatmapsRequest;
+    fn get_beatmaps<F, T>(&self, key: T, f: F)
+        -> Box<Future<Item = Vec<Beatmap>, Error = Error>>
+        where F: FnOnce(GetBeatmapsRequest) -> GetBeatmapsRequest,
+              T: AsRef<str>;
 
     /// Retrieves information about a match.
     ///
@@ -66,7 +80,8 @@ pub trait OsuHyperRequester {
     /// #     try_main().unwrap();
     /// # }
     /// ```
-    fn get_match(&self, key: &str, match_id: u64) -> Result<Match>;
+    fn get_match<T: AsRef<str>>(&self, key: T, match_id: u64)
+        -> Box<Future<Item = Match, Error = Error>>;
 
     /// Retrieves scores for a beatmap.
     ///
@@ -96,8 +111,10 @@ pub trait OsuHyperRequester {
     /// #     try_main().unwrap();
     /// # }
     /// ```
-    fn get_scores<F>(&self, key: &str, beatmap_id: u64, f: F) -> Result<Vec<GameScore>>
-        where F: FnOnce(GetScoreRequest) -> GetScoreRequest;
+    fn get_scores<F, T>(&self, key: T, beatmap_id: u64, f: F)
+        -> Box<Future<Item = Vec<GameScore>, Error = Error>>
+        where F: FnOnce(GetScoreRequest) -> GetScoreRequest,
+              T: AsRef<str>;
 
     /// Retrieves information about a user.
     ///
@@ -127,8 +144,10 @@ pub trait OsuHyperRequester {
     /// #     try_main().unwrap();
     /// # }
     /// ```
-    fn get_user<F, U>(&self, key: &str, user: U, f: F) -> Result<Vec<User>>
-        where F: FnOnce(GetUserRequest) -> GetUserRequest, U: Into<GetBeatmapUser>;
+    fn get_user<F, U>(&self, key: &str, user: U, f: F)
+        -> Box<Future<Item = Vec<User>, Error = Error>>
+        where F: FnOnce(GetUserRequest) -> GetUserRequest,
+              U: Into<GetBeatmapUser>;
 
     /// Retrieves the user's best performances.
     ///
@@ -158,8 +177,11 @@ pub trait OsuHyperRequester {
     ///     println!("{} does not have a best performance", user);
     /// }
     /// ```
-    fn get_user_best<F, U>(&self, key: &str, user: U, f: F) -> Result<Vec<Performance>>
-        where F: FnOnce(GetUserBestRequest) -> GetUserBestRequest, U: Into<GetBeatmapUser>;
+    fn get_user_best<F, T, U>(&self, key: T, user: U, f: F)
+        -> Box<Future<Item = Vec<Performance>, Error = Error>>
+        where F: FnOnce(GetUserBestRequest) -> GetUserBestRequest,
+              T: AsRef<str>,
+              U: Into<GetBeatmapUser>;
 
     /// Retrieves information about a user's recent plays.
     ///
@@ -190,92 +212,127 @@ pub trait OsuHyperRequester {
     /// #     try_main().unwrap();
     /// # }
     /// ```
-    fn get_user_recent<F, U>(&self, key: &str, user: U, f: F) -> Result<Vec<RecentPlay>>
-        where F: FnOnce(GetUserRecentRequest) -> GetUserRecentRequest, U: Into<GetBeatmapUser>;
+    fn get_user_recent<F, T, U>(&self, key: T, user: U, f: F)
+        -> Box<Future<Item = Vec<RecentPlay>, Error = Error>>
+        where F: FnOnce(GetUserRecentRequest) -> GetUserRecentRequest,
+              T: AsRef<str>,
+              U: Into<GetBeatmapUser>;
 }
 
-impl OsuHyperRequester for Client {
-    fn get_beatmaps<F>(&self, key: &str, f: F) -> Result<Vec<Beatmap>>
-        where F: FnOnce(GetBeatmapsRequest) -> GetBeatmapsRequest {
+impl<B, C: Connect> OsuHyperRequester for Client<C, B>
+    where B: Stream<Error = HyperError> + 'static, B::Item: AsRef<[u8]> {
+    fn get_beatmaps<F, T>(&self, key: T, f: F)
+        -> Box<Future<Item = Vec<Beatmap>, Error = Error>>
+        where F: FnOnce(GetBeatmapsRequest) -> GetBeatmapsRequest,
+              T: AsRef<str> {
         let mut uri = String::new();
         uri.push_str(API_URL);
         uri.push_str("/get_beatmaps?k=");
-        uri.push_str(key);
+        uri.push_str(key.as_ref());
 
         mutate_uri(&mut uri, f(GetBeatmapsRequest::default()).0);
 
-        let response = self.get(&uri).send()?;
+        let uri = try_uri!(uri);
 
-        serde_json::from_reader(response).map_err(From::from)
+        Box::new(self.get(uri)
+            .and_then(|res| res.body().concat2())
+            .from_err()
+            .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
     }
 
-    fn get_match(&self, key: &str, match_id: u64) -> Result<Match> {
+    fn get_match<T: AsRef<str>>(&self, key: T, match_id: u64)
+        -> Box<Future<Item = Match, Error = Error>> {
         let mut uri = String::new();
         uri.push_str(API_URL);
         uri.push_str("/get_match?k=");
-        uri.push_str(key);
-        write!(uri, "&mp={}", match_id)?;
+        uri.push_str(key.as_ref());
+        let _ = write!(uri, "&mp={}", match_id);
 
-        let response = self.get(&uri).send()?;
+        let uri = try_uri!(uri);
 
-        serde_json::from_reader(response).map_err(From::from)
+        Box::new(self.get(uri)
+            .and_then(|res| res.body().concat2())
+            .from_err()
+            .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
     }
 
-    fn get_scores<F>(&self, key: &str, beatmap_id: u64, f: F) -> Result<Vec<GameScore>>
-        where F: FnOnce(GetScoreRequest) -> GetScoreRequest {
+    fn get_scores<F, T>(&self, key: T, beatmap_id: u64, f: F)
+        -> Box<Future<Item = Vec<GameScore>, Error = Error>>
+        where F: FnOnce(GetScoreRequest) -> GetScoreRequest,
+              T: AsRef<str> {
         let mut uri = String::new();
         uri.push_str(API_URL);
         uri.push_str("/get_scores?k=");
-        uri.push_str(key);
-        write!(uri, "&b={}", beatmap_id)?;
+        uri.push_str(key.as_ref());
+        let _ = write!(uri, "&b={}", beatmap_id);
 
         mutate_uri(&mut uri, f(GetScoreRequest::default()).0);
 
-        let response = self.get(&uri).send()?;
+        let uri = try_uri!(uri);
 
-        serde_json::from_reader(response).map_err(From::from)
+        Box::new(self.get(uri)
+            .and_then(|res| res.body().concat2())
+            .from_err()
+            .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
     }
 
-    fn get_user<F, U>(&self, key: &str, user: U, f: F) -> Result<Vec<User>>
-        where F: FnOnce(GetUserRequest) -> GetUserRequest, U: Into<GetBeatmapUser> {
+    fn get_user<F, U>(&self, key: &str, user: U, f: F)
+        -> Box<Future<Item = Vec<User>, Error = Error>>
+        where F: FnOnce(GetUserRequest) -> GetUserRequest,
+              U: Into<GetBeatmapUser> {
         let mut uri = String::new();
         uri.push_str(API_URL);
         uri.push_str("/get_user?k=");
-        uri.push_str(key);
+        uri.push_str(key.as_ref());
 
         mutate_uri(&mut uri, f(GetUserRequest::default()).user(user.into()).0);
 
-        let response = self.get(&uri).send()?;
+        let uri = try_uri!(uri);
 
-        serde_json::from_reader(response).map_err(From::from)
+        Box::new(self.get(uri)
+            .and_then(|res| res.body().concat2())
+            .from_err()
+            .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
     }
 
-    fn get_user_best<F, U>(&self, key: &str, user: U, f: F) -> Result<Vec<Performance>>
-        where F: FnOnce(GetUserBestRequest) -> GetUserBestRequest, U: Into<GetBeatmapUser> {
+    fn get_user_best<F, T, U>(&self, key: T, user: U, f: F)
+        -> Box<Future<Item = Vec<Performance>, Error = Error>>
+        where F: FnOnce(GetUserBestRequest) -> GetUserBestRequest,
+              T: AsRef<str>,
+              U: Into<GetBeatmapUser> {
         let mut uri = String::new();
         uri.push_str(API_URL);
         uri.push_str("/get_user_best?k=");
-        uri.push_str(key);
+        uri.push_str(key.as_ref());
 
         mutate_uri(&mut uri, f(GetUserBestRequest::default()).user(user.into()).0);
 
-        let response = self.get(&uri).send()?;
+        let uri = try_uri!(uri);
 
-        serde_json::from_reader(response).map_err(From::from)
+        Box::new(self.get(uri)
+            .and_then(|res| res.body().concat2())
+            .from_err()
+            .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
     }
 
-    fn get_user_recent<F, U>(&self, key: &str, user: U, f: F) -> Result<Vec<RecentPlay>>
-        where F: FnOnce(GetUserRecentRequest) -> GetUserRecentRequest, U: Into<GetBeatmapUser> {
+    fn get_user_recent<F, T, U>(&self, key: T, user: U, f: F)
+        -> Box<Future<Item = Vec<RecentPlay>, Error = Error>>
+        where F: FnOnce(GetUserRecentRequest) -> GetUserRecentRequest,
+              T: AsRef<str>,
+              U: Into<GetBeatmapUser> {
         let mut uri = String::new();
         uri.push_str(API_URL);
         uri.push_str("/get_user_recent?k=");
-        uri.push_str(key);
+        uri.push_str(key.as_ref());
 
         mutate_uri(&mut uri, f(GetUserRecentRequest::default()).user(user.into()).0);
 
-        let response = self.get(&uri).send()?;
+        let uri = try_uri!(uri);
 
-        serde_json::from_reader(response).map_err(From::from)
+        Box::new(self.get(uri)
+            .and_then(|res| res.body().concat2())
+            .from_err()
+            .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
     }
 }
 
